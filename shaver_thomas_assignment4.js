e4b19ed55a2713135dfd552cfc93b11cb9d6f2d9
Thomas Shaver <tmshvr@fullsail.edu>
2012-08-20 17:11:04 -0500

    Added stringToNumber function and wrapped library testing code in a try...catch statement for error handling.

--- shaver_thomas_assignment4.js
+++ shaver_thomas_assignment4.js
@@ -4,3 +4,32 @@
 // SDI 1208
 // Project 4
 // A re-useable library of string, number and array manipulation functions.
+
+var Library = function() {
+  var changeSeperator = function( string, seperator1, seperator2 ){
+    var str = string.split( seperator1 );
+    return str.join( seperator2 );
+  },
+  stringToNumber = function( string ) {
+    var num = parseInt( string );
+    if( isNaN( num )) {
+      throw "isNaN: " + string + ".";
+    }
+    else {
+      return num;
+    };
+  };
+  return {
+    "changeSeperator": changeSeperator,
+    "stringToNumber": stringToNumber
+  };
+};
+
+var lib = new Library();
+try {
+  console.log( lib.changeSeperator( "Hello, world", ", ", " " ));
+  console.log( lib.stringToNumber( "42" ));
+}
+catch( e ) {
+ alert( e.message );
+};

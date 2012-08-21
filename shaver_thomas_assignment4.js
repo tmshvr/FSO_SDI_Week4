a9a825e7e06644e0cec34df16b54777719a071b5
Thomas Shaver <tmshvr@fullsail.edu>
2012-08-21 16:25:28 -0500

    Added dateDifference function, and Converted arguments to global variables rather than having them as literal values in the function calls.

--- shaver_thomas_assignment4.js
+++ shaver_thomas_assignment4.js
@@ -83,13 +83,24 @@ var Library = function() {
     return list.sort(sBKCallback);
   },
   sBKCallback = function( obj1, obj2 ) {
-    if( obj1[ key ] > obj2[ key ]) {
-      return 1;
+    if( obj1[ key ] < obj2[ key ]) {
+      return -1;
     }
-    else if( obj1[ key ] === obj1[ key ]) {
+    else if( obj1[ key ] === obj2[ key ]) {
       return 0;
     };
-    return -1;
+    return 1;
+  },
+  dateDifference = function( date1, date2, flag ) {
+    var ms, seconds, mins, hours, days;
+    date1 = date1.getTime(); // The numerically greater date.
+    date2 = date2.getTime(); // The numerically lesser date.
+    ms = date1 - date2;
+    seconds = ms / 1000;
+    mins = seconds / 60;
+    hours = mins / 60;
+    days = hours / 24;
+    return [ formatPrecision( hours + "", 2 ), formatPrecision( days + "", 2 )];
   };
   return {
     "changeSeperator": changeSeperator,
@@ -102,23 +113,44 @@ var Library = function() {
     "smallestGreaterThan": smallestGreaterThan,
     "fuzzyMatchPercent": fuzzyMatchPercent,
     "sumArray": sumArray,
-    "sortByKey": sortByKey
+    "sortByKey": sortByKey,
+    "dateDifference": dateDifference
   };
-};
+},
 
-var lib = new Library();
+lib = new Library(),
+change = {
+  "string": "Hello, world!",
+  "seperator": ", ",
+  "joiner": " "
+},
+toNum = {"string": "42" },
+matchHTTP = { "string": "https://www.fullsail.com" },
+matchMail = { "string": "aaa@bbb.ccc" },
+matchPhone = { "string": "123-456-7890" },
+toTitle = { "string": "s d i" },
+formatPrec = { "string": "3.14159", "precision": 2 },
+smallestGT = { "list": [ 0, 2, 3, 5, 7, 13, 14, 19 ], "target": 11 },
+fuzzyPercent = { "base": 5, "test": 5.5, "range": 0.11 },
+sumList = { "list": [ 1, "2", 2, 3, "c", 4, 5, 6, "Seven", 7, 8, 9 ] },
+sLBKey = { "list": [ { "a": 2 }, { "a": 3 }, { "a": 1 } ], "key": "a" },
+dates = { "list": [ new Date( 2012, 11, 22 ), new Date() ] };
+
 try {
-  console.log( lib.changeSeperator( "Hello, world!", ", ", " " ));
-  console.log( lib.stringToNumber( "42" ));
-  console.log( lib.matchURL( "https://www.fullsail.com" ));
-  console.log( lib.matchEmail( "aaa@bbb.ccc" ));
-  console.log( lib.matchPhoneNo( "123-456-7890" ));
-  console.log( lib.toTitleCase( "s d i" ));
-  console.log( lib.formatPrecision( "3.14159", 2 ));
-  console.log( lib.smallestGreaterThan( [ 0, 2, 3, 5, 7, 13, 14, 19 ], 11 ));
-  console.log( lib.fuzzyMatchPercent( 5, 5.5, .11 ));
-  console.log( lib.sumArray( [ 1, "2", 2, 3, "c", 4, 5, 6, "Seven", 7, 8, 9 ] ));
-  console.log( lib.sortByKey( [ { "a": 2 }, { "a": 3 }, { "a": 1 } ], "a" ));
+  console.log( change.string + ": " + lib.changeSeperator( change.string, change.seperator, change.joiner ));
+  console.log( toNum.string + ": " + lib.stringToNumber( toNum.string ));
+  console.log( matchHTTP.string + ": " + lib.matchURL( matchHTTP.string ));
+  console.log( matchMail.string + ": " + lib.matchEmail( matchMail.string ));
+  console.log( matchPhone.string + ": " + lib.matchPhoneNo( matchPhone.string ));
+  console.log( toTitle.string + ": " + lib.toTitleCase( toTitle.string ));
+  console.log( formatPrec.string + ", " + formatPrec.precision + ": " + lib.formatPrecision( formatPrec.string, formatPrec.precision ));
+  console.log( smallestGT.list + ", target=" + smallestGT.target + ": " + lib.smallestGreaterThan( smallestGT.list, smallestGT.target ));
+  console.log( "base=" + fuzzyPercent.base + ", test=" + fuzzyPercent.test + ", range=" + fuzzyPercent.range + ": " +
+      lib.fuzzyMatchPercent( fuzzyPercent.base, fuzzyPercent.test, fuzzyPercent.range ) + " " + fuzzyPercent.base + " within " + ((fuzzyPercent.range + "" ).substring(( fuzzyPercent.range + "" ).indexOf( "." ) + 1 ) + "%." ));
+  console.log( lib.sumArray( sumList.list ));
+  console.log( lib.sortByKey( sLBKey.list, sLBKey.key ));
+  console.log( "There are " + lib.dateDifference( dates.list[ 0 ], dates.list[ 1 ], true ).join( " hours, or: " ) + " days. between " +
+      dates.list[ 0 ] + " and " + dates.list[ 1 ]);
 }
 catch( e ) {
  console.log( "Error: " + e.message );

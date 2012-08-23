655f735ba4da484d3e1a7473a9789c61a86868eb
Thomas Shaver <tmshvr@fullsail.edu>
2012-08-22 22:39:38 -0500

    More commenting, changed the order of some conditionals so the value most likely to fail comes first.

--- shaver_thomas_assignment4.js
+++ shaver_thomas_assignment4.js
@@ -15,13 +15,12 @@ var Library = function() {
     // to the end, or from the start to the character before the first non-number in it's argument.
     var num = parseInt( string );
     if( isNaN( num )) { // If the value is Not a Number: (argument probably didn't start with a number)
-      return null;
+      num = null;
     };
     return num;
   },
   matchURL = function( string ) {
-    // Match a string that follows the pattern:
-    // Can start with http:// or https://, and then can contain www., and then a domain
+    // Match a string that can start with http:// or https://, and then can contain www., and then a domain
     // that can be the characters a-z, 0-9, dot (.), and dash (-), one or more times. After the domain,
     // there can be one or more combinations of a period and then 2-3 characters from a-z.
     var remu = new RegExp( "^[(http|https):\/\/]?(www\.)?[a-z0-9\.\-\/]+(\.[a-z]{2,3})+");
@@ -31,7 +30,7 @@ var Library = function() {
     // Match a string starting with at least 3 characters from A-Z a-z, 0-9, dot (.) underscore (_) and
     // dash (-) , one or more times, and then an @ symbol, and then a domain that can be one or more sets
     // of the characters a-z, 0-9, dot (.), and dash (-). The domain is followed by a period and one or
-    // sets of characters a-z, which can be repeated. 
+    // sets of characters a-z, which can be repeated.
       var rema = new RegExp( '^[A-z0-9\.\_\-]{3,}@[A-z\.\-]+(\.[a-z]{2,3})+$' );
       return rema.test( string );
   },
@@ -81,34 +80,35 @@ var Library = function() {
     var high = base + ( base * percent ), // The highest the test value can be.
         low = base - ( base * percent ); // The lowest the test value can be.
     if( test > base && high > test ) {
-      return "above"; // The test value is 
+      return "above"; // The test value is higher than the base, but within the percentage.
     }
     else if( test < base && low < test ) {
-      return "below";
+      return "below"; // The test value is lower than the base, but within the percentage.
     };
-    return "out of range";
+    return "out of range";   // The test value is too low or too high to be within the percentage.
   },
   sumArray = function( list ) {
-    var sum = 0;
-    for( var i = 0; i < list.length; i++ ) {
-      if( typeof( list[ i ]) === typeof( sum )) {
-        sum += list[ i ];
+    var sum = 0; // The total of all the number values in the array.
+    for( var i = 0; i < list.length; i++ ) { // Look at each value in the list argument,
+      if( typeof( list[ i ]) === typeof( sum )) { // and if it is a number
+        sum += list[ i ]; // then add it to the total.
       };
     };
     return sum;
   },
   sortByKey = function( list, sortKey ) {
     key = sortKey;
+    // Call the Array.sort method with a callback function used to define a custom sorting algorithm.
     return list.sort(sBKCallback);
   },
   sBKCallback = function( obj1, obj2 ) {
-    if( obj1[ key ] < obj2[ key ]) {
-      return -1;
+    if( obj1[ key ] < obj2[ key ]) { // If the number in obj1[ key ] is less than that in obj2[ key ]
+      return -1; // Then obj1 has priority in the sort.
     }
-    else if( obj1[ key ] === obj2[ key ]) {
-      return 0;
+    else if( obj1[ key ] === obj2[ key ]) { // If the number in obj1[ key ] is equal to that in obj2[ key ]
+      return 0; // Then both have the same priority in the sort.
     };
-    return 1;
+    return 1; // At this point, the number in obj2[ key ] is greater than that in obj1[ key ], and obj2 gets priority.
   },
   dateDifference = function( date1, date2 ) {
     // date1 is the numerically greater date; date2 is numerically lesser.
@@ -120,9 +120,10 @@ var Library = function() {
     mins = seconds / 60; // 60 seconds to a minute.
     hours = mins / 60; // 60 mins to an hour.
     days = hours / 24; // 24 hours to a day.
+    // Return an array of whole number values of the number of hours and days between the dates.
     return [ Math.floor( hours ), Math.floor( days )];
   };
-  return {
+  return { // Public methods available to code outside of the Library object.
     "changeSeperator": changeSeperator,
     "stringToNumber": stringToNumber,
     "matchURL": matchURL,
@@ -138,6 +139,7 @@ var Library = function() {
   };
 }, // EOF Library.
 
+// var statement that instantiated Library is still going
 
 lib = new Library(),
 change = {
